b218688e6c42923258762fc9e4317082c91c46b3
YKolyagin <rek2270611@gmail.com>
2021-12-09 20:15:50 +0300

    Laravel - ADMIN->Categories->Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Blog\Admin\CategoryController;
 use App\Http\Controllers\Blog\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,12 +19,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::group(['prefix' => 'blog'], function (){
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
 
+//> Blog Admin
+$groupData = [
+    'prefix' => 'admin/blog'
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index', 'edit', 'store', 'update', 'create'];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+//<
 //Route::resource('rest', RestTestController::class)->names('restTest');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
